3a9912326d8307d40ba8d3dc1abf9ceb00d4e78a
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-03-27 18:34:27 -0500

    Create text_cleaning_101.py

---
+++ text_cleaning_101.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+import numpy as np 
+import nltk
+import re
+
+text=   """
+        Boris Johnson, the prime minister of Britain, on Friday announced that he had tested 
+        positive for the coronavirus. 
+        In a brief video released on Twitter, he shared the basics: 
+        Having developed “mild symptoms — that’s to say, a temperature and a 
+        persistent cough” — he underwent testing and received the bad news. 
+        He will now be “self-isolating” until the illness has run its course.
+        """
+        
+ print(text[:100])
+ 
+ #remove_stop_words
+ #remove_junk_words
+ #remove_punctuation
+ #remove_high_freq_words
+ #remvoe_low_freq_words
+ 
+
+ print(text)
