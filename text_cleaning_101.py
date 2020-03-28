ec5f1dec7d83a5c09adcb9615a19e9ab8437d920
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-03-27 19:21:59 -0500

    Update text_cleaning_101.py

--- text_cleaning_101.py
+++ text_cleaning_101.py
@@ -1,24 +1,31 @@
 import pandas as pd 
 import numpy as np 
 import nltk
+from nltk.corpus import stopwords
 import re
+import string
+import seaborn as sns 
+import matplotlib.pyplot as plt
+%matplotlib inline
 
 text=   """
-        Boris Johnson, the prime minister of Britain, on Friday announced that he had tested 
-        positive for the coronavirus. 
-        In a brief video released on Twitter, he shared the basics: 
-        Having developed “mild symptoms — that’s to say, a temperature and a 
-        persistent cough” — he underwent testing and received the bad news. 
-        He will now be “self-isolating” until the illness has run its course.
-        """
-        
- print(text[:100])
- 
- #remove_stop_words
- #remove_junk_words
- #remove_punctuation
- #remove_high_freq_words
- #remvoe_low_freq_words
- 
+ The New York Times (sometimes abbreviated as the NYT and NYTimes) 
+ is an American newspaper based in New York City with worldwide influence 
+ and readership.[6][7][8] Founded in 1851, the paper has won 127 Pulitzer Prizes, 
+ more than any other newspaper.[9][10] The Times is ranked 18th in the world by 
+ circulation and 3rd in the U.S.[11] Nicknamed "The Gray Lady,"[12] the Times has 
+ long been regarded within the industry as a national "newspaper of record."[13] The paper's motto, 
+ "All the News That's Fit to Print," appears in the upper left-hand corner of the front page.
 
- print(text)
+ """
+len(text)
+
+#REMOVE_PUNCTUATION
+exclude = set(string.punctuation)
+text = ''.join(ch for ch in text if ch not in exclude)
+len(text)
+
+#REMOVE_STOPWORDS
+STOPWORDS = set(stopwords.words('english'))
+text =  ' '.join([word for word in text.split() if word not in STOPWORDS])
+len(text)
