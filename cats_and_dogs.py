1dbd83a82c97706ea5a41c4d31e705d4d6db6449
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-12 13:46:05 -0500

    Update cats_and_dogs.py

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -6,3 +6,47 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import random
 import os
+
+Image_Width=128
+Image_Height=128
+Image_Size=(Image_Width,Image_Height)
+Image_Channels=3
+
+filenames=os.listdir("./dogs-vs-cats/train")
+categories=[]
+for f_name in filenames:
+    category=f_name.split('.')[0]
+    if category=='dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+df=pd.DataFrame({
+    'filename':filenames,
+    'category':categories
+
+  
+  from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,\
+     Dropout,Flatten,Dense,Activation,\
+     BatchNormalization
+model=Sequential()
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(Image_Width,Image_Height,Image_Channels)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(128,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=['accuracy'])
+
+model.summary()
