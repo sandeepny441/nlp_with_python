17593b7c6a28ee54b3706bd6922f5693a439acd1
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-27 06:21:43 -0500

    Update instabase_101.py

--- instabase_101.py
+++ instabase_101.py
@@ -18,3 +18,8 @@ txt = "The rain in Spain"
 x = re.search("\s", txt)
 
 print("The first white-space character is located in position:", x.start())
+
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
