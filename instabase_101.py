3d55243ec1b76cba96aaf90045c2ca227f5ade16
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-27 06:21:16 -0500

    Update instabase_101.py

--- instabase_101.py
+++ instabase_101.py
@@ -3,8 +3,12 @@ import re
 
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
-
+print(x)
 
 txt = "The rain in Spain"
 x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
 print(x)
