4cb22379be6d570f6851ce8525ce0fad990ab095
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-28 06:38:49 -0500

    Update regex_102.py

--- regex_102.py
+++ regex_102.py
@@ -20,3 +20,7 @@ print("The first white-space character is located in position:", x.start())
 txt = "The rain in Spain"
 x = re.search("Portugal", txt)
 print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
