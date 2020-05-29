64e9b3d8bb0ad5e46cba7681ad20355f950eea14
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-29 00:52:27 -0500

    Update regex_103.py

--- regex_103.py
+++ regex_103.py
@@ -26,6 +26,11 @@ txt = "The rain in Spain"
 x = re.split("\s", txt)
 print(x)
 
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
 txt = "The rain in Spain"
 x = re.split("\s", txt, 1)
 print(x)
