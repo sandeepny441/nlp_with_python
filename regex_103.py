dbae1fb46673cefda10df1b99020736e8740469d
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-29 00:51:04 -0500

    Update regex_103.py

--- regex_103.py
+++ regex_103.py
@@ -21,3 +21,11 @@ print("The first white-space character is located in position:", x.start())
 txt = "The rain in Spain"
 x = re.search("Portugal", txt)
 print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 1)
+print(x)
