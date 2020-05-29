52fdd87a26e5247ae221624b0c4c610a8394ab2b
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-29 00:50:53 -0500

    Update regex_103.py

--- regex_103.py
+++ regex_103.py
@@ -17,3 +17,7 @@ txt = "The rain in Spain"
 x = re.search("\s", txt)
 
 print("The first white-space character is located in position:", x.start())
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
