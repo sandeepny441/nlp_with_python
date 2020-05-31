74e8466bf3033205fdcbfc265affa91ff23b4815
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-30 20:20:15 -0500

    Update regex_104.py

--- regex_104.py
+++ regex_104.py
@@ -27,6 +27,11 @@ txt = "The rain in Spain"
 x = re.split("\s", txt)
 print(x)
 
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+
 txt = "The rain in Spain"
 x = re.split("\s", txt)
 print(x)
