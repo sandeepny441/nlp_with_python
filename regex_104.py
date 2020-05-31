92da2546ed3487a44dd95151ac2c07a9257c9c30
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-30 20:19:58 -0500

    Update regex_104.py

--- regex_104.py
+++ regex_104.py
@@ -39,3 +39,13 @@ print(x)
 txt = "The rain in Spain"
 x = re.sub("\s", "9", txt, 2)
 print(x)
+
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
