5cfaed7b322beae9ee0773f754d64b4f383f0068
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-05-31 19:11:59 -0500

    Update regex_105.r

--- regex_105.r
+++ regex_105.r
@@ -21,3 +21,10 @@ print("The first white-space character is located in position:", x.start())
 txt = "The rain in Spain"
 x = re.search("Portugal", txt)
 print(x)
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
