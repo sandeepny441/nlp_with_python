456f53d55ab307af2dfa58e8e7e156b67890a99f
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-05 10:00:40 -0500

    Update regex_105.r

--- regex_105.r
+++ regex_105.r
@@ -18,15 +18,7 @@ x = re.search("\s", txt)
 
 print("The first white-space character is located in position:", x.start())
 
-txt = "The rain in Spain"
-x = re.search("Portugal", txt)
-print(x)
-txt = "The rain in Spain"
-x = re.search(r"\bS\w+", txt)
-print(x.group())
 
-txt = "The rain in Spain"
-x = re.split("\s", txt)
 print(x)
 
 txt = "The rain in Spain"
