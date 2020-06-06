19b31c4f7be6088521953b60a4d447104b8946fa
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-06 00:01:37 -0500

    Update regex_106.py

--- regex_106.py
+++ regex_106.py
@@ -8,3 +8,11 @@ x = re.search("^The.*Spain$", txt)
 txt = "The rain in Spain"
 x = re.findall("ai", txt)
 print(x)
+
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.search("\s", txt)
