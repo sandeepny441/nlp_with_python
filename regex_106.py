23f87770850dd5e7eefc92d2283e3547e96ed3a5
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-09 23:52:19 -0500

    Update regex_106.py

--- regex_106.py
+++ regex_106.py
@@ -89,3 +89,8 @@ print(x) #this will print an object
 txt = "The rain in Spain"
 x = re.search("ai", txt)
 print(x) #this will print an object
+
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
