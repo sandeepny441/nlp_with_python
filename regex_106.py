7b1ff1f62ed78871bc6214b596c7143c01c01641
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-15 09:05:25 -0500

    Update regex_106.py

--- regex_106.py
+++ regex_106.py
@@ -147,3 +147,8 @@ print(x) #this will print an object
 txt = "The rain in Spain"
 x = re.search("ai", txt)
 print(x) #this will print an object
+
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
