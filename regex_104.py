86a03c81567dc4410fd7b66cb8f88486cbe94c1b
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-28 13:04:33 -0500

    Update regex_104.py

--- regex_104.py
+++ regex_104.py
@@ -54,3 +54,13 @@ print(x) #this will print an object
 txt = "The rain in Spain"
 x = re.search(r"\bS\w+", txt)
 print(x.string)
+
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
